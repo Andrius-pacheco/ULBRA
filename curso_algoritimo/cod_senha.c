eb02d826bcc13cd299c7ddc4c06d652e4acc8a23
Andrius-pacheco <andrius.p.l@rede.ulbra.br>
2020-03-24 11:59:26 -0300

    codigo estilo senha

---
+++ curso_algoritimo/cod_senha.c
@@ -0,0 +1,24 @@
+// senha//
+
+#include <stdio.h>
+
+
+
+int main()
+{	
+int senha;	
+
+	printf("Digite a senha:");
+	scanf ("%i", &senha);
+	
+		if (senha==1412)
+		{
+		    printf("ola elisangela seja bem vinda!");
+		}
+		else 
+		{
+		    ("acesso negado!");
+		}
+	
+	return 0;
+}
